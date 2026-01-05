a37b74745b64e270560fd0f6abe597e3dbbe0a8f
Samir Bogati <samir23bogati@gmail.com>
2026-01-05 09:23:14 +0545

    camel case solved

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import blogRoutes from "../routes/blogRoutes.js";
+import blogRoutes from "../routes/blogroutes.js";
 import path from "path";
 
 dotenv.config();
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import blogRoutes from "./routes/blogRoutes.js";
+import blogRoutes from "./routes/blogroutes.js";
 
 dotenv.config();
 const app = express();
